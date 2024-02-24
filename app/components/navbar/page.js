0b7f5f865e0df1af9de5ebccbbe5f24b4a07b0ce
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-24 16:32:55 +0300

    navbar component created

--- app/components/navbar/page.js
+++ app/components/navbar/page.js
@@ -1,7 +1,26 @@
-import React from "react";
-
+import styles from "./navbar.module.css";
+import Link from "next/link";
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <ul className={styles.list}>
+          <li className={styles.link}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/movies">Movies</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/movies">Trending</Link>
+          </li>
+          <li className={styles.link}>
+            <Link href="/movies">New Release</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
