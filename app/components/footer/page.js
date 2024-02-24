c4eb52849dd11824467d360ad08b81d16028a872
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-24 16:32:38 +0300

    footer component created

---
+++ app/components/footer/page.js
@@ -0,0 +1,12 @@
+import styles from "./footer.module.css";
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>Footer</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
