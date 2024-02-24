ded9d5cec6379af3d51b0340b99beb66b5fdaa08
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-02-24 16:33:20 +0300

    hero component imported

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
+import Hero from "./components/hero/page";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <section>
+        <Hero />
+      </section>
+    </main>
+  );
 }
