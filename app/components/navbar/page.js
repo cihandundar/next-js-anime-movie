0340f8facc523aa8a2df84217d9c6f9479d9c7a6
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-03-04 17:21:51 +0300

    responsive fix

--- app/components/navbar/page.js
+++ app/components/navbar/page.js
@@ -1,21 +1,39 @@
+"use client";
+import { useState } from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className={styles.nav}>
       <div className={styles.container}>
-        <ul className={styles.list}>
-          <li className={styles.link}>
+        <div className={styles.mobileToggle} onClick={toggleMenu}>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
+        <ul className={`${styles.list} ${isMenuOpen ? styles.showMenu : ""}`}>
+          <li className={styles.link} onClick={closeMenu}>
             <Link href="/">Home</Link>
           </li>
-          <li className={styles.link}>
+          <li className={styles.link} onClick={closeMenu}>
             <Link href="/movies">Movies</Link>
           </li>
-          <li className={styles.link}>
-            <Link href="/movies">Trending</Link>
+          <li className={styles.link} onClick={closeMenu}>
+            <Link href="/trending">Trending</Link>
           </li>
-          <li className={styles.link}>
-            <Link href="/movies">New Release</Link>
+          <li className={styles.link} onClick={closeMenu}>
+            <Link href="/new-release">New Release</Link>
           </li>
         </ul>
       </div>
